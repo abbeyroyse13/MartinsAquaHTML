ccbd96fc079e68d4306bf3556a13ee06a0db8aaa
abbeyroyse13 <heatherruan126@gmail.com>
2021-05-27 10:10:27 -0500

    added a small bit of javascript

---
+++ scripts/database.js
@@ -0,0 +1,16 @@
+const database = {
+    fish: [{
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        },
+        {
+            name: "Jerome",
+            species: "Clownfish",
+            image: "https://cdn.mos.cms.futurecdn.net/4UdEs7tTKwLJbxZPUYR3hF-1200-80.jpg",
+            harvestLocation: "Singapore",
+            diet: "Algae",
+            length: "21 in"
+        }
+    ]
+}
